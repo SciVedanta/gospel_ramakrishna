a6655261c8fd47e198ee07b32ba5253b4a7a7640
Kamal Choudhary <writetokamal.1989@gmail.com>
2023-09-09 01:15:27 -0400

    Add files via upload

---
+++ gospel_ramakrishna/make_chaps.py
@@ -0,0 +1,15 @@
+f=open('b90bfb242d7240f2ab2048109719471a.md','r')
+lines=f.read().splitlines()
+f.close()
+count=0
+tmp=[]
+for i,ii in enumerate(lines):
+  tmp.append(ii)
+  if '**Chapter' in ii:
+    ff='Chapter-'+str(count)+'.md'
+    fname=open(ff,'w')
+    tmp='\n'.join(tmp)
+    fname.write(tmp)
+    fname.close()
+    count+=1
+    tmp=[]
